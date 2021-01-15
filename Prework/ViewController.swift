884eeae0ccb262be17d4e5ed730679ac0b965b6d
Nour Elabbasy <nour102600@gmail.com>
2021-01-15 02:08:03 -0500

    keyboard

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.billAmountTextField.becomeFirstResponder()
         // Do any additional setup after loading the view.
     }
 
