71145a3cc2e4e4edefa7ac8648b46876f667b3dd
Nour Elabbasy <nour102600@gmail.com>
2021-01-15 20:35:26 -0500

    App Icon

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.billAmountTextField.becomeFirstResponder()
+        
         // Do any additional setup after loading the view.
     }
 
